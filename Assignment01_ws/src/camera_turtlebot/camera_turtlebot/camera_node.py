5e86cfe17b340303dce2db379925799757ecb9ea
NilsB98 <nils.braehmer@gmx.de>
2022-04-27 03:32:17 +0200

    added param support, video stream and images can now be saved

--- Assignment01_ws/src/camera_turtlebot/camera_turtlebot/camera_node.py
+++ Assignment01_ws/src/camera_turtlebot/camera_turtlebot/camera_node.py
@@ -2,63 +2,66 @@ import rclpy
 import os
 import cv2
 from rclpy.node import Node
+from rclpy.parameter import Parameter
+from rcl_interfaces.msg import SetParametersResult
 from std_msgs.msg import String
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 import numpy as np
-
-# import picamera
-# import picamera.array
+from datetime import datetime
 
 class Camera(Node):
 
-        def __init__(self):
-                super().__init__('camera')
-                self.publisher_ = self.create_publisher(Image, '/raw_image', 10)
-                self.freq = 2
+    def __init__(self):
+        super().__init__('camera')
+        # publisher for raw img data
+        self.publisher_ = self.create_publisher(Image, '/raw_image', 10)
+        # camera stream
+        self.vid = cv2.VideoCapture(0)
+
+        # ros parameters
+        self.freq = .2  # freq between images to publish [s]
+        self.declare_parameter("fps", int(1 / self.freq))
+        self.add_on_set_parameters_callback(self.on_param_change)
+        # callback timer
+        self.timer = self.create_timer(self.freq, self.callback)
+
+    def callback(self):
+        ret, frame = self.vid.read()
+        bridge = CvBridge()
+
+        try:
+            msg = bridge.cv2_to_imgmsg(frame)
+            self.publisher_.publish(msg)
+            print(f"send new img {str(datetime.now()).split('.')[0]}")
+
+        except CvBridgeError as e:
+            print(e)
+
+    def on_param_change(self, parameters):
+        print("--------------------- PARAM CHANGE ---------------------")
+        for parameter in parameters:
+            if parameter.name == "fps":
+                # calc new frequency
+                fps = parameter.value
+                print(f"changed fps from {1 / self.freq} to {fps}")
+                self.freq = 1 / fps
+                # reinitialize timer
+                tmp = self.timer
                 self.timer = self.create_timer(self.freq, self.callback)
-                self.vid = cv2.VideoCapture(0)
+                tmp.destroy()
+                print("recreated timer")
+                return SetParametersResult(successful=True)
 
-        def callback(self):
-                #print(os.getcwd())
-
-                # img_msg = Image(data =)
-                # take image TODO:
-
-
-
-                # publish image
-                # empty_img = np.zeros((10,10,3))
-                # empty_img[:, :3, 1] = 240
-                # empty_img = empty_img / 255
-                # print(empty_img.shape)
-                ret, frame = self.vid.read()
-                bridge = CvBridge()
-
-
-
-                try:
-                        msg = bridge.cv2_to_imgmsg(frame)
-                        self.publisher_.publish(msg)
-                        print(msg)
-
-                except CvBridgeError as e:
-                        print(e)
-
-        def capture(self):
-                with picamera.PiCamera() as camera:
-                        with picamera.array.PiRGBArray(camera) as output:
-                                camera.capture(output, 'rgb')
-                                print('Captured %dx%d image' % (output.array.shape[1], output.array.shape[0]))
-                                return output.array
-
-
+        return SetParametersResult(successful=False)
 
 
 def main(args=None):
-        rclpy.init(args=args)
-        rclpy.spin(Camera())
-        rclpy.shutdown()
+    rclpy.init(args=args)
+    rclpy.spin(Camera())
+
+    cap.release()
+    rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
--- Assignment01_ws/src/image_display/image_display/image_display_node.py
+++ Assignment01_ws/src/image_display/image_display/image_display_node.py
@@ -1,30 +1,72 @@
 import rclpy
 from rclpy.node import Node
+from rclpy.parameter import Parameter
+from rcl_interfaces.msg import SetParametersResult
 from std_msgs.msg import String
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 import numpy as np
 import cv2
+import os
+from datetime import datetime
 
 class ImgDisplayNode(Node):
     def __init__(self):
         super().__init__('img_display_node')
-        # self.publisher_ = self.create_publisher(Image, '/proc_img', 10)
-        self.subscriber_img_ = self.create_subscription(Image, '/proc_img', self.callback, 10)
+        # subscribe to the topic of processed images
+        self.subscriber_img_ = self.create_subscription(Image, '/proc_img', self.display_img_data, 10)
 
-    def callback(self, msg):
+        # set parameters and detect a parameter change
+        self.store_imgs = False
+        self.show_stream = True
+        self.declare_parameter("store_imgs", value=self.store_imgs)
+        self.declare_parameter("show_stream", value=self.show_stream)
+        self.add_on_set_parameters_callback(self.on_param_change)
+
+    def display_img_data(self, msg):
         # display the image data
         bridge = CvBridge()
         img_msg = bridge.imgmsg_to_cv2(msg)
-        cv2.imshow("display", img_msg)
-        print("Received data:")
-        print(img_msg)
+
+        if self.show_stream:
+            cv2.imshow("display", img_msg)
+            cv2.waitKey(1)
+
+        if self.store_imgs:
+            path = "../imgs"
+            if not os.path.exists(path):
+                os.makedirs(path)
+
+            cv2.imwrite(os.path.join(path, str(datetime.now()).replace(' ', '_')+'.jpg'), img_msg)
+
+        print(f"{img_msg.shape}: {str(datetime.now()).split('.')[0]}")
+
+    def on_param_change(self, parameters):
+        for parameter in parameters:
+
+            if parameter.name == "store_imgs":
+                store_imgs = parameter.value
+                print(f"changed store_imgs from {self.store_imgs} to {store_imgs}")
+                self.store_imgs = store_imgs
+                return SetParametersResult(successful=True)
+
+            elif parameter.name == "show_stream":
+                show_stream = parameter.value
+                print(f"changed store_imgs from {self.show_stream} to {show_stream}")
+                self.show_stream = show_stream
+                cv2.destroyAllWindows()
+                return SetParametersResult(successful=True)
+
+            else:
+                print(f"unknown/unused parameter {parameter.name}")
+                return SetParametersResult(successful=False)
 
 
 def main(args=None):
     rclpy.init(args=args)
     rclpy.spin(ImgDisplayNode())
 
+    cv2.destroyAllWindows()
     rclpy.shutdown()
 if __name__ == '__main__':
     main()
