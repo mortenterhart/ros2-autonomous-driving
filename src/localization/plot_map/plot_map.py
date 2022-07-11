1fee43560e7df78169e65c68dd6729ec9e0fc189
JetteJarl <knopp.henriette@gmail.com>
2022-07-11 15:52:16 +0200

    print recieved msg in plot_map

--- src/localization/plot_map/plot_map.py
+++ src/localization/plot_map/plot_map.py
@@ -6,8 +6,8 @@ class Plot_map(Node):
 
         self.subscriber_cones_ = sef.create_subscription(Float32MultiArray, '/known_cones', sef.receive_cones, 10)
 
-    def receive_cones():
-        pass
+    def receive_cones(self, msg):
+        print(msg)
 
 def main(args=None):
     rclpy.init(args=args)
