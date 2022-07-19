b447511b5c21e2e3b1e14df3ebe147917f95dee2
Morten Terhart <morten.terhart@gmail.com>
2022-07-19 17:22:38 +0200

    Add script parameter to stop automatic build

--- AVAI_turtlebot.sh
+++ AVAI_turtlebot.sh
@@ -1,5 +1,10 @@
 # Script to start nodes run on the turtlebot
 
+# Build packages unless an option is specified
+if [ "$1" != "--no-build" ]; then
+    colcon build --packages-select camera_turtlebot
+fi
+
 # source package and nodes
 . install/setup.bash
 
@@ -12,6 +17,3 @@ mkdir -p logs
 ros2 launch turtlebot3_bringup robot.launch.py > logs/log_bringup.txt 2>&1 &   # start bringup
 ros2 run camera_turtlebot camera > logs/log_camera.txt 2>&1 &               # start camera
 ros2 run camera_turtlebot processing > logs/log_processing.txt 2>&1 &       # start processing
-
-
-
