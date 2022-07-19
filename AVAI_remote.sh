b5e3da67ace231ca3c9b01116fc60703bae2f659
Morten Terhart <morten.terhart@gmail.com>
2022-07-19 17:13:10 +0200

    Redirect stderr to log files in startup scripts

--- AVAI_remote.sh
+++ AVAI_remote.sh
@@ -9,6 +9,6 @@ export PYTHONPATH=$PWD/src/perception/perception:$PYTHONPATH
 # create logs directory
 mkdir -p logs
 
-ros2 run perception cone_detection > logs/log_cone_detection.txt &    # start cone detection
-ros2 run localization localization > logs/log_localization.txt &      # start localization node
-ros2 run localization plot_map > logs/log_map_detection.txt &         # start map
+ros2 run perception cone_detection > logs/log_cone_detection.txt 2>&1 &    # start cone detection
+ros2 run localization localization > logs/log_localization.txt 2>&1 &      # start localization node
+ros2 run localization plot_map > logs/log_map_detection.txt 2>&1 &         # start map
--- AVAI_turtlebot.sh
+++ AVAI_turtlebot.sh
@@ -9,9 +9,9 @@ export TURTLEBOT3_MODEL="${MODEL_NAME}"
 # create logs directory
 mkdir -p logs
 
-ros2 launch turtlebot3_bringup robot.launch.py > logs/log_bringup.txt &   # start bringup
-ros2 run camera_turtlebot camera > logs/log_camera.txt &               # start camera
-ros2 run camera_turtlebot processing > logs/log_processing.txt &       # start processing
+ros2 launch turtlebot3_bringup robot.launch.py > logs/log_bringup.txt 2>&1 &   # start bringup
+ros2 run camera_turtlebot camera > logs/log_camera.txt 2>&1 &               # start camera
+ros2 run camera_turtlebot processing > logs/log_processing.txt 2>&1 &       # start processing
 
 
 
