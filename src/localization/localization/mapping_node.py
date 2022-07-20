894a1119f86e93ddb7ab86ae29f4d2f5a591f259
Morten Terhart <morten.terhart@gmail.com>
2022-07-20 20:07:21 +0200

    Add explaining comments to mapping node

--- src/localization/localization/mapping_node.py
+++ src/localization/localization/mapping_node.py
@@ -32,16 +32,22 @@ class MappingNode(Node):
     def __init__(self):
         super().__init__('mapping_node')
 
+        # Subscriber for potential cones and robot position and
+        # publisher for known cones
         self.subscriber_potential_cones = self.create_subscription(Float32MultiArray, '/potential_cones', self.receive_cones, 10)
         self.subscriber_robot_pos = self.create_subscription(Float32MultiArray, '/robot_pos', self.receive_robot_pos, 10)
         self.publisher_known_cones = self.create_publisher(Float32MultiArray, '/known_cones', 10)
 
+        # Parameters for cone detection
         self.BUFFER_LENGTH = 5
         self.TRACKING_RADIUS = 0.15
 
+        # Cone buffer for the last few messages and list
+        # of confirmed cones
         self.cone_buffer = FlexibleQueue(self.BUFFER_LENGTH)
         self.known_cones = []
 
+        # Robot position
         self.robot_pos = []
 
         # plot settings
@@ -52,13 +58,14 @@ class MappingNode(Node):
         # plt.ylim(0, 4)
 
     def receive_robot_pos(self, msg):
+        # Set current robot position for plotting
         self.robot_pos = np.array(msg.data)
 
     def receive_cones(self, msg):
-        cones = np.array(msg.data).reshape((-1, msg.layout.dim[1].size))
+        potential_cones = np.array(msg.data).reshape((-1, msg.layout.dim[1].size))
 
-        # add cones to buffer
-        self.cone_buffer.push(cones)
+        # Add new potential cone positions to buffer
+        self.cone_buffer.push(potential_cones)
 
         self.track_cones()
         self.plot_cones()
@@ -81,27 +88,40 @@ class MappingNode(Node):
         self.publisher_known_cones.publish(known_cones_msg)
 
     def track_cones(self):
+        # Start cone detection first when the buffer has enough frames
         if len(self.cone_buffer) < self.BUFFER_LENGTH:
             return
 
-        # cone appearing in all buffers
+        # Collect cones that are detected in all buffer frames
         detected_cones = []
 
+        # Find nearest neighbors of cones from the first buffer in
+        # all subsequent buffers
         for cone in self.cone_buffer.queue[0]:
             found_cones = [[cone[1], cone[2]]]
 
+            # For every following buffer, check if a cone within a
+            # certain neighborhood exists
             for next_buffer in self.cone_buffer.queue[1:]:
                 next_cone = self.find_cone_neighbor_in_list(cone, next_buffer)
 
+                # If no neighboring position is found in this buffer, this cone
+                # is not considered as an actual cone
                 if next_cone is None:
                     break
 
+                # Add the neighboring cone
                 found_cones.append([next_cone[1], next_cone[2]])
 
+            # If a neighboring cone is found for every buffer, this cone
+            # is detected as an actual cone and the mean position is saved
+            # as the cone position
             if len(found_cones) == self.BUFFER_LENGTH:
                 mean_cone_pos = np.mean(found_cones, axis=0)
                 detected_cones.append(np.array([cone[0], mean_cone_pos[0], mean_cone_pos[1]]))
 
+        # Check if the detected cones are already known and
+        # only add these cones which are unknown yet
         for detected_cone in detected_cones:
             known_cone = self.find_cone_neighbor_in_list(detected_cone, self.known_cones)
 
@@ -116,6 +136,11 @@ class MappingNode(Node):
         return None
 
     def compare_cones(self, cone_a, cone_b):
+        """
+        Compare two cones and check if the labels
+        match and if their Euclidean distance is less
+        # than the tracking threshold.
+        """
         # Return false if the labels do not match
         if int(cone_a[0]) != int(cone_b[0]):
             return False
