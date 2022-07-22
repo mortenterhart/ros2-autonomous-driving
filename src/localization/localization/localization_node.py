bc5e6979c3879f6da3b0189d13b0670c042e73a4
JetteJarl <knopp.henriette@gmail.com>
2022-07-22 08:34:42 +0200

    changed QoS for odometry subscriber to 10

--- src/localization/localization/localization_node.py
+++ src/localization/localization/localization_node.py
@@ -39,7 +39,7 @@ class LocalizationNode(Node):
         self.subscriber_scan = self.create_subscription(LaserScan, 'scan', self.received_scan,
                                                         qos_profile=qos_profile_sensor_data)
         self.subscriber_odom = self.create_subscription(Odometry, 'odom', self.receive_odom,
-                                                        qos_profile=qos_profile_sensor_data)
+                                                        10)
         # Publisher for known cones
         self.publisher_cones = self.create_publisher(Float32MultiArray, '/potential_cones', 10)
         self.publisher_pos = self.create_publisher(Float32MultiArray, '/robot_pos', 10)
@@ -119,6 +119,9 @@ class LocalizationNode(Node):
             odom.pose.pose.orientation.z,
             odom.pose.pose.orientation.w
         )[2]
+
+        # print(f"{np.array(self.orientation)*180/np.pi}")
+        # self.orientation = self.orientation[2]
 
         stamp = float(f"{odom.header.stamp.sec}.{odom.header.stamp.nanosec}")
         self.odom_buffer.push(np.array([self.pos[0], self.pos[1], self.orientation, stamp]))
